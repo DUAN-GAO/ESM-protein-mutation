06c1d797bfa983191e9fd29b0bc61dc1c0c01133
DUAN GAO <1481407566@qq.com>
2025-11-25 11:14:02 +0800

    s

--- run.py
+++ run.py
@@ -1,67 +1,76 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
+import sys
 import gzip
 
-def open_vcf(path, mode="rt"):
-    """自动识别是否为 gz 文件"""
+PROTEIN_ALTERING = {
+    "missense_variant",
+    "synonymous_variant",
+    "stop_gained",
+    "stop_lost",
+    "frameshift_variant",
+    "inframe_insertion",
+    "inframe_deletion",
+    "start_lost",
+}
+
+def parse_csq(csq_string):
+    csq_entries = csq_string.split(",")
+    consequences = []
+    for entry in csq_entries:
+        fields = entry.split("|")
+        if len(fields) > 1:
+            consequences.append(fields[1])
+    return consequences
+
+
+def is_protein_affecting(csq_string):
+    consequences = parse_csq(csq_string)
+    return any(cons in PROTEIN_ALTERING for cons in consequences)
+
+
+def open_file(path, mode="rt"):
     if path.endswith(".gz"):
         return gzip.open(path, mode)
-    else:
-        return open(path, mode)
-
-def is_protein_coding(info_field):
-    """判断 CSQ 是否包含 protein_coding 相关注释"""
-    if "CSQ=" not in info_field:
-        return False
-
-    raw = info_field.split("CSQ=")[1]
-    entries = raw.split(",")
-
-    # protein_coding 判定关键字
-    keywords = ["protein_coding", "missense", "synonymous", "stop_gained", "start_lost"]
-
-    for entry in entries:
-        if any(k in entry for k in keywords):
-            return True
-
-    return False
+    return open(path, mode)
 
 
-def split_vcf(vcf_path, out_pc="protein_coding.vcf", out_nonpc="non_protein_coding.vcf"):
-    f_out_pc = open(out_pc, "w")
-    f_out_non = open(out_nonpc, "w")
+def main(input_vcf, out_protein, out_nonprotein):
+    fin = open_file(input_vcf, "rt")
+    fout_pro = open_file(out_protein, "wt")
+    fout_non = open_file(out_nonprotein, "wt")
 
-    with open_vcf(vcf_path, "rt") as f:
-        for line in f:
-            if line.startswith("#"):
-                # 头部写入两个文件
-                f_out_pc.write(line)
-                f_out_non.write(line)
-                continue
+    for line in fin:
+        if line.startswith("#"):
+            fout_pro.write(line)
+            fout_non.write(line)
+            continue
 
-            parts = line.strip().split("\t")
-            if len(parts) < 8:
-                f_out_non.write(line)
-                continue
+        fields = line.strip().split("\t")
+        info = fields[7]
 
-            info = parts[7]
+        csq_string = None
+        for item in info.split(";"):
+            if item.startswith("CSQ="):
+                csq_string = item[4:]
+                break
 
-            if is_protein_coding(info):
-                f_out_pc.write(line)
-            else:
-                f_out_non.write(line)
+        if csq_string is None:
+            fout_non.write(line)
+            continue
 
-    f_out_pc.close()
-    f_out_non.close()
+        if is_protein_affecting(csq_string):
+            fout_pro.write(line)
+        else:
+            fout_non.write(line)
+
+    fin.close()
+    fout_pro.close()
+    fout_non.close()
 
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description="按 protein-coding 注释拆分 VCF 文件")
-    parser.add_argument("--vcf", required=True, help="输入 VCF 文件")
-    parser.add_argument("--out_pc", default="protein_coding.vcf", help="输出 protein_coding VCF")
-    parser.add_argument("--out_nonpc", default="non_protein_coding.vcf", help="输出 non-protein-coding VCF")
-    args = parser.parse_args()
+    if len(sys.argv) < 4:
+        print("Usage: python split_by_exonic_status.py input.vcf protein.vcf nonprotein.vcf")
+        sys.exit(1)
 
-    split_vcf(args.vcf, args.out_pc, args.out_nonpc)
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
